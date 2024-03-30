aad3eaeed7085f310455804f24b6b295cba8fae3
Daniel <93657978+Daniel-Andrawis@users.noreply.github.com>
2024-03-30 16:04:42 -0400

    :)

---
+++ test.ps1
@@ -0,0 +1,6 @@
+Set-Culture de-DE
+Set-WinSystemLocale -SystemLocale de-DE
+Set-WinUILanguageOverride -Language de-DE
+Set-WinUserLanguageList de-DE -Force
+Set-WinHomeLocation -GeoId 94
+shutdown -r;exit
